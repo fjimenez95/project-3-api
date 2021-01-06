d0d73dd48f9bdf322ae509dec8dd859502666e1e
Freddy Jimenez <freddy.junior@gmail.com>
2021-01-05 22:32:03 -0500

    adds signup function

---
+++ controllers/users.js
@@ -0,0 +1,17 @@
+// REQUIRE MODULES
+const User = require('../models/user');
+
+// HANDLE EXPORTS
+module.export = {
+    signup
+};
+
+// DEFINE A SIGNUP CONTROLLER ACTION
+async function signup(req, res) {
+    try {
+        const user = new User(req.body);
+        res.json({ user })
+    } catch (err) {
+        res.status(404).json(err);
+    }
+}
