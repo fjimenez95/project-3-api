c44480bc2c8fa8393d6377defe5ae4bedddb677f
Freddy Jimenez <freddy.junior@gmail.com>
2021-01-05 22:22:30 -0500

    adds working server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const logger = require('morgan');
+
+const port = process.env.PORT || 3002;
+
+const app = express();
+
+require('dotenv').config();
+require('./config/database');
+
+// MOUNT MIDDLEWARE
+app.use(express.json());
+app.use(logger('dev'));
+
+
+// MOUNT ROUTES
+// app.use('/api/users', require('./routes/api/users'));
+
+// TELL APP TO LISTEN
+app.listen(port, function() {
+    console.log(`Express is listening on ${port}...`);
+})
