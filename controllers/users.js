cc78faa73efbb6e0b23c114d5df5ca37a9143e56
Freddy Jimenez <freddy.junior@gmail.com>
2021-01-06 22:20:47 -0500

    added jwt web token for signup

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,7 @@
 // REQUIRE MODULES
 const User = require('../models/user');
+const SECRET = process.env.SECRET;
+const jwt = require('jsonwebtoken');
 
 // HANDLE EXPORTS
 module.exports = {
@@ -9,9 +11,20 @@ module.exports = {
 // DEFINE A SIGNUP CONTROLLER ACTION
 async function signup(req, res) {
     try {
-        const user = new User.create(req.body);
-        res.json({ user })
+        const user = await User.create(req.body);
+        const token = createJWT(user);
+        res.json({ token });
     } catch (err) {
         res.status(404).json(err);
     }
 }
+
+// HELPER FUNCTION FOR GENERATING JWTs
+function createJWT(user) {
+    return jwt.sign(
+      {user: user}, 
+      SECRET, 
+      {expiresIn: '24h'},
+    );
+    // If the key value pair is the same value as above, you only need to do { user }
+  }
